b65bfae87e738b99c32c09ec8213aeccec330bc3
Alexey Lymarev <alymarev@sinergo.ru>
2021-05-12 23:31:26 +0700

    added tab system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-study-project';
+
+  selectedTab = 'recipe';
+
+  onNavigate(tabName: string) {
+    this.selectedTab = tabName;
+  }
 }
